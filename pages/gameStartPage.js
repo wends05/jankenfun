c396768bcfafb9280024bd3941a0c10b9c75a168
Wendell Dador <cxnner05@gmail.com>
2023-10-06 19:39:06 +0800

    8

--- pages/gameStartPage.js
+++ pages/gameStartPage.js
@@ -1,28 +1,39 @@
 
 
+
+
+
+
+
+
 var images = ["/menuFiles/timer3.png", "/menuFiles/timer2.png", "/menuFiles/timer1.png"];
+    index = 0;
 
-index = 0;
-
-timer = setInterval(() => {
+    timer = setInterval(() => {
     if (index < 3) {
-    const timer = document.getElementById('timer');
-    timer.src =images[index];
+    const time = document.getElementById('timer');
+    time.src =images[index];
     index = index + 1;
     } else {
-        console.log("hello");
-        addBackButton();
-        clearInterval(timer)
+        haha();
     }
 }, 1000);
 
-function addBackButton() {
-    var cont = document.getElementById('containerr');
-    
+let back = document.getElementById("tae");
+
+function haha() {
+    console.log("hello");
+
+    var container = document.getElementById('containerr');
+    const back = document.createElement("button");
+
+    back.classList.add("bg-[url('/menuFiles/bck.png')]", "h-40", "w-60", "bg-center", "bg-contain", "bg-no-repeat");
+    back.id = "tae"
+    clearInterval(timer);
+    container.removeChild(document.getElementById('timer')); 
+    container.appendChild(back)
+
+    back.addEventListener("click" , function() {
+        window.location = '../mainMenu.html'
+    })
 }
-
-
-
-
-
-
